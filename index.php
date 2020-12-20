3218f5d396671f8b8c63413a8e057456dc689d14
PankajiBirds <75002307+PankajiBirds@users.noreply.github.com>
2020-12-20 18:38:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
   echo "Hello World";
+  $host='ec2-52-6-75-198.compute-1.amazonaws.com';
+  $db = 'dq22oqbhhje8j';
+  $username = 'btsgokzekbpgts';
+  $password = '922d82db920a404f4cffebde604ca8816473d638c959de7670c4dadc1c60c9dd';
+
+  $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+
+  try{
+    // create a PostgreSQL database connection
+    $conn = new PDO($dsn); 
+    // display a message if connected to the PostgreSQL successfully
+    if($conn){
+      echo "Connected to the <strong>$db</strong> database successfully!";
+    }
+  }catch (PDOException $e){
+    // report error message
+    echo $e->getMessage();
+  }
 ?>
